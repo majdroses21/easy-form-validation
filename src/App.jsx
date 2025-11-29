09cf942a8a0ccc06698c86de7009feca373ad859
Majd Roses <majdrose@gmail.com>
2025-11-30 00:23:43 +0300

    Adding programming logic: Performing the validation process

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,131 @@
+import { useState } from "react";
 import "./App.css";
 import Alert from "./components/test/Alert";
 import FormBtn from "./components/ui/FormBtn";
 import FormInput from "./components/ui/FormInput";
 import FormSelect from "./components/ui/FormSelect";
+//State
 function App() {
-  const expectedSalaries = ['2000$', '3000$', '4000$', '5000$']
+  const expectedSalaries = ["2000$", "3000$", "4000$", "5000$"];
+  let [showAlert, setShowAlert] = useState(true);
+  let [alertType, setAlertType] = useState("success");
+  let [errors, setError] = useState([]);
+
+  let [formData, setFormData] = useState({
+    name: "",
+    age: 0,
+    phonenumber: 0,
+    isEmployee: false,
+    salary: null,
+  });
+  //validation
+  const required = (value, fieldName) => {
+    if (!value) {
+      return `the ${fieldName} is required`
+    } else {
+      return null;
+    }
+  };
+  const ageBetween = (age) => {
+    if (age >= 18 && age <= 40) {
+      return null;
+    } else {
+      return "Age must be between 18 and 40"
+    }
+  };
+  const phonenumber = (number) => {
+    let digitCount = String(number).length;
+    if (digitCount >= 9 && digitCount <= 12) {
+     return null;
+    } else {
+      return "The Phone Number must be between 9 & 12";
+    }
+  };
+  // Submet The Form
+  const handelFormSubmit = () => {
+    // تجميعع الأخطاء
+    const validationErrors = [
+      required(formData.name, "Name"),
+      required(formData.age, "Age"),
+      required(formData.phonenumber, "PhoneNumber"),
+      ageBetween(formData.age),
+      phonenumber(formData.phonenumber)
+    ].filter(erroe => erroe !== null)
+
+    //
+    if (validationErrors.length > 0) {
+      setError(validationErrors);
+      setShowAlert(true);
+      setAlertType('error');
+    }else{
+      setShowAlert(true);
+      setAlertType('success');
+      setError(['Form sent Sucsessfuly'])
+    }
+  }
   return (
     <div className="App">
       <form className="form-app" action="">
-        <FormInput name='name' label="Name" placeholder="name" type="text" />
-        <FormInput name='phonenumber' label="Phonenumber" placeholder="Enter your phonenumber" type="number" />
-        <FormInput name='age' label="Age" placeholder="Enter your age" type="number" />
-        <FormInput name='employee' label="Are you employee?" type="checkbox" />
-        <FormSelect name='formSelect' label='Expected Salary' options={expectedSalaries} />
-        <FormBtn label='Submit' type='button' />
-        <Alert message="This is an error alert" type="error" />
-        <Alert message="This is a success alert" type="success" />
+        <FormInput
+          name="name"
+          label="Name"
+          placeholder="name"
+          type="text"
+          onChange={(e) => {
+            setFormData({ ...formData, name: e.target.value });
+          }}
+        />
+        <FormInput
+          name="phonenumber"
+          label="Phonenumber"
+          placeholder="Enter your phonenumber"
+          type="number"
+          onChange={(e) => {
+            setFormData({ ...formData, phonenumber: e.target.value });
+          }}
+        />
+        <FormInput
+          name="age"
+          label="Age"
+          placeholder="Enter your age"
+          type="number"
+          onChange={(e) => {
+            setFormData({ ...formData, age: e.target.value });
+          }}
+        />
+        <FormInput
+          name="employee"
+          label="Are you employee?"
+          type="checkbox"
+          onChange={(e) => {
+            setFormData({ ...formData, isEmployee: e.target.checked });
+          }}
+        />
+        <FormSelect
+          name="formSelect"
+          label="Expected Salary"
+          options={expectedSalaries}
+          onChange={(e) => {
+            setFormData({ ...formData, salary: e.target.value });
+          }}
+        />
+        <FormBtn
+          onClick={() => {
+            handelFormSubmit();
+          }}
+          label="Submit"
+          type="button"
+        />
+     {
+      showAlert && Array.isArray(errors) && errors.length > 0 && (
+        errors.map((textMsg, i) => 
+          textMsg && textMsg.toString().trim() !== '' ? (
+            <Alert key={i} message={textMsg} type={alertType} />
+          ) : null
+        ).filter(Boolean) // إزالة العناصر null
+      )
+    }
+        
       </form>
     </div>
   );
